e84d38a22a18f763c0e46704c67768d8840aff77
Jamie Ha <jamieha9@gmail.com>
2018-06-09 12:02:30 +1000

    9/06/18: Applied the webserver update camera update email update main

--- Email.py
+++ Email.py
@@ -1,3 +1,5 @@
+#@author Jamie Ha
+#created: 17/03/18
 import threading
 import time
 import smtplib
@@ -8,6 +10,9 @@ from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import Encoders
 '''
+CHECK BEFORE THE COMPLETED PROJECT:
+        - Test if the threading of the email and main files work together
+        - Test if it records and sends the video
 Have the email service run through this class
 Have perminent email addresses usrname&passwrds in the module
 Sources:
@@ -15,13 +20,14 @@ Sources:
 - http://naelshiab.com/tutorial-send-email-python/
 - https://www.tutorialspoint.com/python3/python_sending_email.html
 '''
-# Make sure the this class is threaded/processed
+# The email is threaded for optimal efficiency.
+# Check if this module works
 class Emails(threading.Thread):
     host = 'smtp.gmail.com'
     port = 465# port for gmail
-    login = 'e9261733@gmail.com'
+    login = 'e9261733@gmail.com'# email login for entering in the gmail webserver
     password = 'emailpassword'
-    clients = ['e94282861@gmail.com']
+    clients = ['e94282861@gmail.com']# recipients
     localTime = time.asctime(time.localtime(time.time()))
 
     def __init__(self, message, orginal_path):
@@ -31,7 +37,7 @@ class Emails(threading.Thread):
         #The message of the email
 
     def sendVideo(self):
-        # find a way to send a video over the gmail webserver
+        # Send a video over through email
         mainMessage = 'Video Recorded:- ' + Emails.localTime
         filename = 'Video.avi'
         try:
@@ -55,6 +61,7 @@ class Emails(threading.Thread):
             pass
 
     def sendMail(self, frame):
+        # send the email with frame
         lgEmail = Log(Emails.localTime, self.path)
         try:
             msg = MIMEMultipart()# sending multiple attachments e.g. images, texts
@@ -82,5 +89,14 @@ class Emails(threading.Thread):
             sendVideo()
         elif function_to_call == "sendMail":
             sendMail(frame)
-
         threadLock.release()# end the thread
+
+'''
+Status:
+runFunc:
+- Test if the thread releases or not
+sendVideo func:
+- Testing this function
+sendMail func:
+- Make sure this works
+'''
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 import numpy
 import threading# Use for email
 import cv2 as cv
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
 from Camera import Camera
 from picamera import PiCamera
 from logFiles import Log
@@ -17,15 +17,16 @@ from picamera.array import PiRGBArray
 Inspired by Hacker Shack:
 -   https://github.com/HackerShackOfficial/Smart-Security-Camera
 '''
-# Modify this and clean this up
 original_path = '/home/pi/Desktop/FrontDoorDetectProject/'
 DateNTime = time.asctime(time.localtime(time.time()))# Time stamp on the image
 logEmailSent = Log(DateNTime, original_path)
 em = Emails(Format.message, original_path)# getting the image captured
 piVCam = VideoCamera(orginal_path, DateNTime)
 previousTime = 0# temporary storage of the time
+app = Flask(__name__)# creation of Webserver
 
 def get_PersonInFrame():
+    # Stop sending emails of person after 9 minutes
     global previousTime
     while True:
         frame, found_person = piVCam.get_PersonInFrame()
@@ -39,11 +40,48 @@ def get_PersonInFrame():
             em.runFunc("sendMail", piVCam.SaveImage())# Put the email sending into a thread
             previousTime = 0
 
+@app.route('/')
+def mainFunctions():
+    # Title of the webpage
+    templateData = {
+        'title': 'Raspberry Pi Camera Feed'
+    }
+    return render_template('index.html', **templateData)# puts it into the main html file
+'''
+CamFeed and live_stream; from Hacker Shack
+'''
+def camFeed(camera):
+    # Uploads the frame to webserver
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/live_stream')
+def live_stream():# shows the live stream of the camera
+    return Response(camFeed(video_camera),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/live_stream/record_stream/<recording>')
+def record():
+    # recording the live stream here when button is pressed or not
+    record = False
+    message = ''
+    if recording == "on":
+        message = "Recording"
+        record = True
+    elif recording == "off":
+        message = "Not recording"
+        record = False
+
+    templateData = {
+        'message': message,
+        'record': record
+    }
+    return render_template('index.html', **templateData)# put in the main html file
+
 if __name__ == '__main__':
-    t = threading.Thread(target=get_PersonInFrame, args=())# thread this
-    t.daemon = True
+    t = threading.Thread(target=get_PersonInFrame, args=())
+    t.daemon = True #
     t.start()
-    #threading.start_new_thread(get_PersonInFrame, None)
-    #threading.daemon()# work on this
-    # apply all multithreading applications here
-    # Webserver and email are multithreaded
+    app.run(host='0.0.0.0', debug=True)
